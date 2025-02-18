9215e259a25df8ad031c776f9a8ece01826039c1
basemomar94 <basemomar2012@gmail.com>
2025-02-18 13:42:14 +0200

    version upgrade

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -86,7 +86,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = (System.currentTimeMillis() / 1000).toInt()
-        versionName = "0.1.0"
+        versionName = "0.1.0.1"
     }
 
     flavorDimensions += "mode"
